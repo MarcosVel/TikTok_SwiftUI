b4df547a942fb3e371c59ae9bdbb1fe143cbe71c
MarcosVel <marcosvelosodev@gmail.com>
2025-06-17 00:42:51 -0300

    Initial Commit

---
+++ TikTokSwiftUI/TikTokSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokSwiftUIApp.swift
+//  TikTokSwiftUI
+//
+//  Created by Marcos Roberto Lemos Veloso on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TikTokSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
